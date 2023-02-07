5ed3f1157405bf4d78542c007101861139fc6a19
Erick Sebastián Navarrete Cruz <esnavarrete@ciencias.unam.mx>
2023-02-07 01:01:17 -0600

    add method tmap a letter with plugboard

--- plugboard.py
+++ plugboard.py
@@ -2,3 +2,10 @@ class Plugboard:
 
     def __init__(self, settings) -> None:
         self.settings = settings
+
+    def map_letter(self, letter) -> str:
+        if letter in self.settings.keys():
+            new_letter = self.settings[letter]
+        else:
+            new_letter = letter
+        return new_letter
