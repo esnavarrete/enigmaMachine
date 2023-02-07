62d2734c43a2e89fd4f48df1a784bb2de0b79711
Erick Sebastián Navarrete Cruz <esnavarrete@ciencias.unam.mx>
2023-02-07 01:32:36 -0600

    adding a method for rotors to map a letter

--- rotors.py
+++ rotors.py
@@ -5,6 +5,12 @@ class Rotor:
         self.wiring = wiring
         self.notch = notch
 
+    def map_letter(self, letter) -> str:
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        letter_index = alphabet.index(letter)
+        new_letter = self.wiring[letter_index]
+        return new_letter
+
     def rotate(self, steps):
         print(f'una rotación de {steps}')
         return
