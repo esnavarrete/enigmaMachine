b76e2447afc59966c5ed35df79aae217f54992d7
Erick Sebastián Navarrete Cruz <esnavarrete@ciencias.unam.mx>
2023-04-11 19:51:22 -0600

    rotor implementation complete (i think)

--- rotors.py
+++ rotors.py
@@ -1,16 +1,19 @@
+from settings import ALPHABET
+
+
 class Rotor:
 
     def __init__(self, init_position, wiring, notch) -> None:
-        self.init_position = init_position
+        self.position = init_position
         self.wiring = wiring
         self.notch = notch
 
     def map_letter(self, letter) -> str:
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        letter_index = alphabet.index(letter)
-        new_letter = self.wiring[letter_index]
+        letter_position = ALPHABET.index(letter)
+        new_letter = self.wiring[(
+            letter_position + self.position) % len(ALPHABET)]
         return new_letter
 
-    def rotate(self, steps):
-        print(f'una rotación de {steps}')
+    def rotate(self):
+        self.position = (self.position + 1) % len(ALPHABET)
         return
